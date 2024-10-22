40844d8cfc65f9fc7abef907d511665de76b0e1a
Camilla Avila <avilacamilla@outlook.com>
2024-10-22 15:40:43 -0300

    feature: Ajusta responsividade do container InformationSection

--- app/components/home/informations/InformationsSection.vue
+++ app/components/home/informations/InformationsSection.vue
@@ -2,46 +2,46 @@
 </script>
 
 <template>
-  <div class="flex flex-row justify-center items-center bg-blue-500 p-8">
-    <UContainer class="flex justify-around w-full">
+  <div class="flex justify-center items-center bg-blue p-8">
+    <UContainer class="flex flex-wrap justify-center w-full gap-6">
       <div class="flex flex-col items-center">
-        <UNuxtImg
+        <NuxtImg
           src="/icons/hands.svg"
           alt="Clientes"
           class="w-16 h-16 mb-4"
         />
-        <span class="text-white font-bold">+ de 600 clientes</span>
+        <span class="text-white font-bold text-center">+ de 600 clientes</span>
       </div>
       <div class="flex flex-col items-center">
-        <UNuxtImg
+        <NuxtImg
           src="/icons/brasil.svg"
           alt="Estados Brasileiros"
           class="w-16 h-16 mb-4"
         />
-        <span class="text-white font-bold">Presente em <strong>23</strong> estados brasileiros</span>
+        <span class="text-white font-bold text-center">Presente em <strong>23</strong> estados brasileiros</span>
       </div>
       <div class="flex flex-col items-center">
-        <UNuxtImg
-          src="/icons/times.svg"
-          alt="Pacientes"
-          class="w-16 h-16 mb-4"
-        />
-        <span class="text-white font-bold">+ de 3 milhões de pacientes atendidos</span>
-      </div>
-      <div class="flex flex-col items-center">
-        <UNuxtImg
+        <NuxtImg
           src="/icons/exames.svg"
           alt="Exames"
           class="w-16 h-16 mb-4"
         />
-        <span class="text-white font-bold">10 milhões de exames</span>
+        <span class="text-white font-bold text-center">10 milhões de exames</span>
+      </div>
+      <div class="flex flex-col items-center">
+        <NuxtImg
+          src="/icons/time.svg"
+          alt="Pacientes"
+          class="w-16 h-16 mb-4"
+        />
+        <span class="text-white font-bold text-center">+ de 3 milhões de pacientes atendidos</span>
       </div>
     </UContainer>
   </div>
 </template>
 
 <style scoped>
-  .bg-blue-500 {
-    background-color: #2c7cb3; /* Azul da imagem */
+  .bg-blue {
+    background-color: #2c7cb3;
   }
 </style>
